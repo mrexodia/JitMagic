c99f032930e4ab971810c3693789bf54c7fdb241
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2018-08-13 18:39:38 +0200

    move around projects for a more logical structure

---
+++ ManagedJitTest/ManagedJitTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedJitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("ManagedJitTest");
+        }
+    }
+}
